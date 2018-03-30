7e45d01c35733843d01143131bf951c09a9f8f92
Jes Ritchie <jesritchie@hotmail.com>
2018-03-30 13:05:01 -0300

    getting-started preload

--- Getting-Started/PreloadJS/js/main.js
+++ Getting-Started/PreloadJS/js/main.js
@@ -1,3 +1,12 @@
 /*
 		Using PreloadJS	
 */
+ function loadImage() {
+        var preload = new createjs.LoadQueue();
+        preload.addEventListener("fileload", handleFileComplete);
+        preload.loadFile("preloadjs-bg-center.png");
+      }
+
+      function handleFileComplete(event) {
+        document.body.appendChild(event.result);
+      }
