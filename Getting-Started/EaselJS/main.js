1bb7d212137fca11726705a72f414ef35bcce753
Jes Ritchie <jesritchie@hotmail.com>
2018-03-29 22:39:02 -0300

    Finished getting started Easel JS tutorial

--- Getting-Started/EaselJS/main.js
+++ Getting-Started/EaselJS/main.js
@@ -12,10 +12,52 @@
 	// Then use shape's graphics API to give it color and bounds. 
 	// Position it on the canvas with x and y coordinates, and add it to the stage.
 	var circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	//Face circle
+	circle.graphics.beginFill("Pink").drawCircle(100, 100, 200);
 	circle.x = 100;
 	circle.y = 100;
 	stage.addChild(circle);
+	//Color in some
+	circle.graphics.beginFill("RGB(222,223,228)").drawCircle(100, 100, 190);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//Smile
+	circle.graphics.beginFill("White").drawCircle(100, 100, 100);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//Cover some of circle to a make smile
+	circle.graphics.beginFill("RGB(222,223,228)").drawCircle(100, 80, 100);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//left eye 
+	circle.graphics.beginFill("Brown").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//left cover
+	circle.graphics.beginFill("RGB(222,223,228)").drawCircle(0, 10, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//Right eye 
+	circle.graphics.beginFill("Brown").drawCircle(200, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//Right eye cover
+	circle.graphics.beginFill("RGB(222,223,228)").drawCircle(200, 10, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//nose
+	circle.graphics.beginFill("Pink").drawCircle(100, 50, 10);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	//nose
 	//Finally, we need to update our stage to display the shape we just added.
 	stage.update();
   }
